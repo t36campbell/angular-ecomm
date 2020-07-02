15a269216e461ce98ee49c5636701dd3a45350df
t36campbell <campbell3636@yahoo.com>
2020-07-01 22:10:51 -0400

    stopping for the night

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -67,6 +67,7 @@ const config = {
       libraries: ['places']
     }), //agm google maps
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
--- src/app/components/stores/stores.component.ts
+++ src/app/components/stores/stores.component.ts
@@ -1,131 +1,85 @@
-import { Component, OnInit, ViewChild, ElementRef, NgZone, } from '@angular/core';
-import { MapsAPILoader, MouseEvent } from '@agm/core';
+import { Component, NgZone } from '@angular/core';
 @Component({
   selector: 'app-stores',
   templateUrl: './stores.component.html',
   styleUrls: ['./stores.component.scss']
 })
-export class StoresComponent implements OnInit {
-  latitude: number;
-  longitude: number;
+export class StoresComponent {
+  lat: number;
+  lng: number;
   zoom: number;
-  address: string;
-  private geoCoder;
+  radius: number; 
+  address: Object;
+  formattedAddress: string;
   markers: marker[] = [
 	  {
       lat: 40.7060361, 
       lng: -74.0088256,
-		  label: 'A',
-		  draggable: false
+      label: 'A'
 	  },
 	  {
       lat: 25.7620955, 
       lng: -80.1932258,
-		  label: 'B',
-		  draggable: false
+      label: 'B',
 	  },
 	  {
       lat: 44.9750472, 
       lng: -93.2503777,
-		  label: 'C',
-		  draggable: false
+      label: 'C',
     },
     {
       lat: 32.7793704, 
       lng: -96.8008565,
-		  label: 'D',
-		  draggable: false
-	  },
+	    label: 'D',
+		  },
 	  {
       lat: 47.6172481, 
       lng: -122.3520857,
-		  label: 'E',
-		  draggable: false
+      label: 'E',
 	  },
 	  {
       lat: 34.1015088, 
       lng: -118.333556,
-		  label: 'F',
-		  draggable: false
+      label: 'F',
 	  }
-  ]
+  ];
 
-  @ViewChild('search')
-  public searchElementRef: ElementRef;
+  constructor(public zone: NgZone) { }
 
-
-  constructor(
-    private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
-  ) { }
-
-
-  ngOnInit() {
-    //load Places Autocomplete
-    this.mapsAPILoader.load().then(() => {
-      this.setCurrentLocation();
-      this.geoCoder = new google.maps.Geocoder;
-
-      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
-      autocomplete.addListener("place_changed", () => {
-        this.ngZone.run(() => {
-          //get the place result
-          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          //verify result
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          //set latitude, longitude and zoom
-          this.latitude = place.geometry.location.lat();
-          this.longitude = place.geometry.location.lng();
-          this.zoom = 12;
-        });
-      });
-    });
+  getAddress(place: object) {
+    this.address = place['formatted_address'];
+    this.formattedAddress = place['formatted_address'];
+    this.zone.run(() => this.formattedAddress = place['formatted_address']);
   }
 
-  // Get Current Location Coordinates
-  private setCurrentLocation() {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.zoom = 8;
-        this.getAddress(this.latitude, this.longitude);
-        this.markers.push({
-          lat: this.latitude,
-          lng: this.longitude,
-          draggable: false
-        });
-      });
-    }
-  }
-
-  getAddress(latitude, longitude) {
-    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
-      console.log(results);
-      console.log(status);
-      if (status === 'OK') {
-        if (results[0]) {
-          this.zoom = 12;
-          this.address = results[0].formatted_address;
-        } else {
-          window.alert('No results found');
-        }
-      } else {
-        window.alert('Geocoder failed due to: ' + status);
+  geolocate() {
+    if (navigator.geolocation) {
+      const options = {
+        maximumAge: 60000,
+        timeout: 5000,
+        enableHighAccuracy: true
       }
-
-    });
-  }
-
-}
-
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const newMarker = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+          this.lat = newMarker.lat;
+          this.lng = newMarker.lng;
+          this.markers.push(newMarker);
+          this.zoom = 12;
+        },
+        err => {
+          console.log(err)
+        },
+        options
+      )
+    };
+  };
+};
 interface marker {
   lat: number;
   lng: number;
   label?: string;
-  draggable: boolean;
-}
+};
