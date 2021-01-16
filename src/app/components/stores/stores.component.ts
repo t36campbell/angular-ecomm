8635794d1bbab01d15a06d7b84461b5fca78d37c
t36campbell <campbell3636@yahoo.com>
2021-01-16 17:03:21 -0500

    built prod and deployed

--- src/app/components/stores/stores.component.ts
+++ src/app/components/stores/stores.component.ts
@@ -117,7 +117,7 @@ export class StoresComponent implements OnInit {
         options
       );
     }
-  }  
+  }
   onAutocompleteSelected(result: PlaceResult) {
     console.log('onAutocompleteSelected: ', result);
   }
